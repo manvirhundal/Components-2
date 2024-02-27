3b2a860a8daa71b51d7d96600d7a95f0d6067382
manvir kaur <manvirkaur2121@gmail.com>
2024-02-27 18:14:22 -0500

    i did it

--- src/components/ProductCardContainer.jsx
+++ src/components/ProductCardContainer.jsx
@@ -1,12 +1,20 @@
 import ProductCard from "./ProductCard";
 
-export default function ProductCardContainer() {
+export default function ProductCardContainer({products}) {
   return (
     <>
       <h1>Discover Your Favorites</h1>
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      {products.map((product, index) => (
+        <ProductCard
+          key={index}
+          name={product.name}
+          description={product.description}
+          price={product.price}
+          imageUrl={product.imageUrl}
+          isInStock={product.isInStock}
+          style={{ marginBottom: '20px' }} // Add some spacing between cards
+        />
+      ))}
     </>
   );
 }
